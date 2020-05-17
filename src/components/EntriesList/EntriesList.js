fcf3df5df11b5d982278cf43d678005fe25a853d
fedor <fedor.malyshkin@yandex.ru>
2020-05-18 00:06:48 +0400

    Fixes

--- src/components/EntriesList/EntriesList.js
+++ src/components/EntriesList/EntriesList.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import Entry from './Entry/Entry';
 import './EntriesList.css';
 
 const EntriesList = (props) => {
-
     let elements = []
+    const [states, updateStates] = useState({})
+
+    function updateCheckbox(state, index) {
+        let newState = {}
+        newState[index] = state
+        updateStates({...states, ...newState})
+    }
 
     if (props.data) {
-        elements = props.data.map(entry => {
+        elements = props.data.map((entry, index) => {
+            let checkboxEnabled = false
+            if (states[index])
+                checkboxEnabled = true
+
             return (
-                <li key={entry.name}>
-                    <input type="checkbox"/>
-                    <Entry data={entry} format_url={props.format_url}/>
+                <li key={index}>
+                    <input type="checkbox" checked={checkboxEnabled} disabled={true}/>
+                    <Entry data={entry} formatUrl={props.format_url}
+                           clickHandler={(state) => updateCheckbox(state, index)}/>
                 </li>)
         });
     }
